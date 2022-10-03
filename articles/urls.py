1c408c30a5da18ebab22029c09be7cd39c4bd540
Roholla Ramezani <rohollaramezani@gmail.com>
2022-10-03 09:24:19 +0330

    added paginator to article page

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,5 @@ app_name='articles'
 urlpatterns = [
     path('',views.articles,name='articles'),
     path('<int:article_id>',views.single_page,name='article'),
+    path('article/send',views.send,name='article.send'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse,Http404
 from django.shortcuts import render,get_object_or_404
 from .models import Article
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def articles(request):
-    articles=Article.objects.order_by('-created_at')[:10]
+    articles=Article.objects.order_by('-created_at')
+    paginator=Paginator(articles,2)
+    page=request.GET.get('page')
+    articles=paginator.get_page(page)
     return render(request,'articles/index.html',{
         'title':'Articles',
         'articles':articles,
+        'paginator':paginator,
         'mydata': [1,2,3,4],
         'strdata':'iran food',
         'number':24.23654
@@ -25,3 +30,8 @@ def single_page(request,article_id):
         'title':article.title,
         'article':article
     })
+
+
+def send(request):
+    return HttpResponse(request.POST.get('title'))
+    return render(request,'articles/send.html')
